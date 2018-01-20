132036e8ec4699c9b4113dec0535c73d354d9087
Brooke Mitchell <mitchell.brooke@gmail.com>
2018-01-21 10:09:36 +1300

    Chpt 8 react exercises

--- exercises/chapter8/src/Main.purs
+++ exercises/chapter8/src/Main.purs
@@ -66,12 +66,11 @@ addressBook :: forall props. ReactClass props
 addressBook = createClass $ spec initialState \ctx -> do
   AppState { person: Person person@{ homeAddress: Address address }, errors } <- readState ctx
 
-  let renderValidationError err = D.li' [ D.text err ]
+  let renderValidationError err = D.div [ P.className "alert alert-danger" ] [ D.text err ]
 
       renderValidationErrors [] = []
       renderValidationErrors xs =
-        [ D.div [ P.className "alert alert-danger" ]
-                [ D.ul' (map renderValidationError xs) ]
+        [ D.div [] (map renderValidationError xs)
         ]
 
       formField name hint value update =
